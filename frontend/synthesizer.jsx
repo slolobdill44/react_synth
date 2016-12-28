23f549a4f9d65f0f959a23600ff155ac8aa02a9f
slolobdill44 <adrianlobdill@clifford-3.gateway.pace.com>
2016-12-28 14:47:17 -0800

    Sets up skeleton of synth project

---
+++ frontend/synthesizer.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDom from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", function() {
+  ReactDom.render(<Synth />, document.getElementById('root'));
+});
